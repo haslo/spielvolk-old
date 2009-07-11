79327bd2177550f98046f9da1f34dbb507922ca6
Guido Gloor Modjib <haslo@haslo.ch>
2009-07-11 19:00:53 +0200

    Yay, Capistrano works (with Ruby 1.8.6)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,29 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :deploy_to, "/home/user60/rails"
+set :application, "spielvolk.ch"
+set :user, "user60"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+role :web, "user60@moria.invisible.ch"
+role :app, "user60@moria.invisible.ch"
+role :db,  "user60@moria.invisible.ch", :primary => true
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+set :use_sudo, false
+set :keep_releases, 4
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+set :scm, :git
+set :branch, "master"
+set :repository, "git@github.com:haslo/spielvolk.git"
+set :deploy_via, :remote_cache
+
+namespace :deploy do
+  desc "Create the database yaml file"
+  # Restart passenger on deploy
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
